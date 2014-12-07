ff3d52f1949905890a24af9c80e5c1ba62dca9eb
Douglas Mendes Barreto <mendes-barreto@live.com>
2014-12-07 14:36:32 -0200

    Teste de Loading feito

---
+++ Assets/Scripts/Com/Globo/Sitio/Games/MenuState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AquelaFrameWork.Core.State;
+
+public class MenuState : AState
+{
+    public override void BuildState()
+    {
+        System.Threading.Thread.Sleep(5000);
+        
+        base.BuildState();
+    }
+}
